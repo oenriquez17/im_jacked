cb68927716d86eef9519fba8803e8d3886af5456
Oscar Enriquez <oscar.enriquez@centralsquare.com>
2020-09-02 09:20:00 -0400

    Added models folder/files and postgres dependency

---
+++ Models/Exercise.js
@@ -0,0 +1,8 @@
+class Exercise{
+    constructor(id, name, muscle_worked, usesBodyweight){
+        this.id = id;
+        this.name = name;
+        this.muscle_worked = muscle_worked;
+        this.usesBodyweight = usesBodyweight;
+    }    
+}
---
+++ Models/WorkoutEntry.js
@@ -0,0 +1,10 @@
+class WorkoutEntry{
+    constructor(id, date, exercise, sets, reps, weight){
+        this.id = id;
+        this.date = date;
+        this.exercise = exercise;
+        this.sets = sets;
+        this.reps = reps;
+        this.weight = weight;
+    }
+}

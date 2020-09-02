101b1584c9afac9d63a720d409f87f86b6b10d65
Oscar Enriquez <oscar.enriquez@centralsquare.com>
2020-09-02 10:47:40 -0400

    Added ejs and ejs/navbar test

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/ajax', express.static(__dirname + '/node_modules/ajax/lib'));
+
 app.set('view engine', 'ejs');
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
+});
+
+app.get('/', function(req, res) {
+  res.render('index');
 });

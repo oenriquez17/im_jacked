6b83a4f174d7b08806ef032199232be28871198f
Oscar Enriquez <oscar.enriquez@centralsquare.com>
2020-09-03 10:35:16 -0400

    Successful test getting post data

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-const db = require('./configuration/db_conn');
+// create db connection
+var db = require('./configuration/db_conn');
 
+// create url-encoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// configure file locations
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/ajax', express.static(__dirname + '/node_modules/ajax/lib'));
 app.use('/data', express.static(__dirname + '/data'));
 
+// configure engine to use ejs
 app.set('view engine', 'ejs');
 
+// start app
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
@@ -28,6 +36,8 @@ app.get('/exercise', function(req, res) {
   res.render('exercise');
 });
 
-app.post('/add_edit_exercise', function (req, res) {
+app.post('/add_edit_exercise', urlencodedParser, function (req, res) {
+  console.log("Name: " + req.body.exercise);
+  console.log("Muscle: " + req.body.muscle);
   res.send('POST request to the homepage');
 });

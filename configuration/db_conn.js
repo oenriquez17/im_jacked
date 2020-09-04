911bb233067f9e854fa4bfd3a834b0e92def08f9
Oscar Enriquez <oscar.enriquez.cs@gmail.com>
2020-09-04 15:40:00 -0400

    Added index screen

--- configuration/db_conn.js
+++ configuration/db_conn.js
@@ -2,20 +2,28 @@ require('dotenv').config();
 
 const pg = require('pg');
 
+const isProd = process.env.PROD == 1 ? true : false;
+
+const user_var = isProd ? process.env.DB_USER : process.env.DEV_DB_USER;
+const password_var = isProd ? process.env.DB_PASSWORD : process.env.DEV_DB_PASSWORD;
+const host_var = isProd ? process.env.DB_HOST : process.env.DEV_DB_HOST;
+const port_var = isProd ? process.env.DB_PORT : process.env.DEV_DB_PORT;
+const database_var = isProd ? process.env.DB_DATABASE : process.env.DEV_DB_DATABASE;
+
 const pool = new pg.Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE
+    user: user_var,
+    password: password_var,
+    host: host_var,
+    port: port_var,
+    database: database_var
 });
 
 console.log('*******DB CONFIG*********');
-console.log("User: " + process.env.DB_USER);
-console.log("Pwd: " + process.env.DB_PASSWORD);
-console.log("Host: " + process.env.DB_HOST);
-console.log("Port: " + process.env.DB_PORT);
-console.log("Database: " + process.env.DB_DATABASE);
+console.log("User: " + user_var);
+console.log("Pwd: " + password_var);
+console.log("Host: " + host_var);
+console.log("Port: " + port_var);
+console.log("Database: " + database_var);
 console.log('*************************');
 
 module.exports = {pool}

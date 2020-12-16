9e9bbeed92830c483825504a1e11868f07c008d3
Oscar Enriquez <oscar.enriquez.cs@gmail.com>
2020-12-15 21:39:28 -0500

    Fixed date bug

--- queries/queries.js
+++ queries/queries.js
@@ -13,7 +13,7 @@ const get_all_exercise = 'SELECT * FROM exercises ORDER BY name';
 const get_exercise_details = 'SELECT e.Id, e.name AS name, e.muscle_worked as muscle, MAX(w.weight) AS max, MIN(w.weight) AS min FROM workout_entries w INNER JOIN exercises e ON e.Id = w.exercise GROUP BY e.Id ORDER BY name';
 
 //Selects all workout entries to show detailed progress
-const get_exerciseprogress_details = "SELECT w.weight, w.reps_completed, w.sets_completed, TO_CHAR(w.entry_date, 'YYYY-MM-DD') AS entry_date FROM workout_entries w INNER JOIN exercises e ON e.Id = w.exercise WHERE e.Id = $1 ORDER BY entry_date DESC";
+const get_exerciseprogress_details = "SELECT w.weight, w.reps_completed, w.sets_completed, w.entry_date AS entry_date FROM workout_entries w INNER JOIN exercises e ON e.Id = w.exercise WHERE e.Id = $1 ORDER BY entry_date DESC";
 
 //Selects all workout entries by date (all exercises for single date)
 const get_workoutentries_bydate = "SELECT w.*, e.* FROM workout_entries w INNER JOIN exercises e ON e.Id = w.exercise WHERE entry_date = $1 ORDER BY w.Id";
